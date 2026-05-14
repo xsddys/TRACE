9d3a22309df7e55cb83d314f3b18a58f5721a343
xsddys <765094843@qq.com>
2026-05-14 14:45:48 +0800

    fixed

--- scripts/merge_best_actor.sh
+++ scripts/merge_best_actor.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-BASE_DIR="/mnt/shared-storage-user/wenxiaoyu/hezhida/TROJail/checkpoints/Ablation/Semantic-wo_refulsal-A-q253i-T-l318i-bigger_validation__20260418_194625"
+BASE_DIR=""
 STEPS=(${*:-120})
 
 for step in "${STEPS[@]}"; do
   LOCAL_DIR="${BASE_DIR}/global_step_${step}/actor"
-  python /mnt/shared-storage-user/wenxiaoyu/hezhida/TROJail/scripts/model_merge.py \
+  python ./scripts/model_merge.py \
     --local_dir "${LOCAL_DIR}"
 done
